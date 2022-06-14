9868cc818189a87c464611b003bf3b2d53f35929
firetiredesire <105272632+firetiredesired@users.noreply.github.com>
2022-06-14 13:39:34 +0200

    level 1 v9

--- timetravel/Assets/movement.cs
+++ timetravel/Assets/movement.cs
@@ -4,6 +4,7 @@ using System;
 
 public class movement : MonoBehaviour
 {
+    
     private Rigidbody2D rb;
     public float speed = 20;
     public bool grounded;
@@ -16,6 +17,7 @@ public class movement : MonoBehaviour
     public float boost = 10;
     GameObject cload;
     
+    
 
     void Start()
     {
@@ -27,6 +29,15 @@ public class movement : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        Collider2D collider = collision.collider;
+        bool collideFromLeft = false;
+        bool collideFromTop = false;
+        bool collideFromRight = false;
+        bool collideFromBottom = false;
+        float RectWidth = this.GetComponent<Collider2D>().bounds.size.x;
+        float RectHeight = this.GetComponent<Collider2D>().bounds.size.y;
+        float circleRad = collider.bounds.size.x;
+        
         //Output the Collider's GameObject's name
         print(collision.collider.name);
 
@@ -68,7 +79,7 @@ public class movement : MonoBehaviour
             
         }
         if(collision.collider.name == "button") {
-         buttonactivate1();
+        
         }
         if(collision.collider.name == "boostpad") {
         rb.gravityScale =-1;
@@ -77,7 +88,39 @@ public class movement : MonoBehaviour
         if(collision.collider.name == "-boostpad") {
         rb.gravityScale =20;
         }
+       
+     
         
+        if(collision.collider.name == "button")
+        {
+        Vector3 contactPoint = collision.contacts[0].point;
+        Vector3 center = collider.bounds.center;
+        if (contactPoint.y > center.y && (contactPoint.x < center.x + RectWidth / 2 && contactPoint.x > center.x - RectWidth / 2)) {
+                 collideFromTop = true;
+                 print("TOP");
+             }
+             else if (contactPoint.y < center.y &&
+                 (contactPoint.x < center.x + RectWidth / 2 && contactPoint.x > center.x - RectWidth / 2)) {
+                 collideFromBottom = true;
+                 print("BOttem");
+             }
+             else if (contactPoint.x > center.x &&
+                 (contactPoint.y < center.y + RectHeight / 2 && contactPoint.y > center.y - RectHeight / 2)) {
+                 collideFromRight = true;
+                 print("RIght");
+             }
+             else if (contactPoint.x < center.x &&
+                 (contactPoint.y < center.y + RectHeight / 2 && contactPoint.y > center.y - RectHeight / 2)) {
+                 collideFromLeft = true;
+                 print("LEft");
+             }
+        }
+        
+        if (collideFromBottom) {
+            death();
+        }
+        
+
     }
 
     void bouncyBlock(){
@@ -120,6 +163,7 @@ public class movement : MonoBehaviour
        else 
        {
           speed = 20;
+           
        }
 
         if (Input.GetKey(KeyCode.W))
@@ -150,7 +194,8 @@ public class movement : MonoBehaviour
         }
         
            if(!grounded) {
-              Instantiate (cload, transform.position, cload.transform.rotation);
+               
+              
            }else{
                print("666");
            }
